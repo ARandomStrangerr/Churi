03ce646c6386232112f3e2c7251921980ebdee82
MO <billslim0996@gmail.com>
2024-07-18 01:12:32 -0700

    debug for SignUp function

--- back-end/src/routes/Authentication.js
+++ back-end/src/routes/Authentication.js
@@ -7,12 +7,12 @@ ROUTER.post("/sign-up", SignUp);
 ROUTER.post("/sign-out", SignOut);
 
 async function SignUp(request, response){
-	if (!request.body.username || request.body.username.length === 0 || request.body.password || request.body.password.length === 0 || !request.body.email || request.body.email.length === 0) {
+	if (!request.body.username || request.body.username.length === 0 || !request.body.password || request.body.password.length === 0 || !request.body.email || request.body.email.length === 0) {
 		response.status(400).json({error: "Username, Password, and Email are required"});
 		return;
 	}
 	const salt = await BCRYPT.genSalt(10);
-	const hashedPassword = await BCRYPT.hasedPassword(request.body.password, salt);
+	const hashedPassword = await BCRYPT.hash(request.body.password, salt);
 	if (await DATABASE.signUp(request.body.username, hashedPassword, request.body.email))
 		response.status(200).json({message: "success create account"});
 	else
