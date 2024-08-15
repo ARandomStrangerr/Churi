73895d8dd3572eea36dc0e9844a3a86241ded5fc
thanhdo <billslim0996@gmail.com>
2024-08-15 00:20:22 -0700

    add icons for footer

--- front-end/src/components/StoreFooter.vue
+++ front-end/src/components/StoreFooter.vue
@@ -1,27 +1,32 @@
+<script setup>
+import FacebookIcon from "./icons/Facebook.vue";
+import InstagramIcon from "./icons/Instagram.vue";
+import PinterestIcon from "./icons/Pinterest.vue";
+import MapIcon from "./icons/Map.vue";
+</script>
+
 <template>
 <footer class="regular-main-page-width">
-	<div class="flex-row">
-		<div class="link-containter">
-			<img src="../assets/logo-1.png">
-		</div>
-		<div class="link-containter">
-			<h2>Churi</h2>
-			<div>About Us</div>
-			<div>Contact Us</div>
-		</div>
-		<div class="link-containter">
-			<h2>Help</h2>
-			<div>Return Policy</div>
-			<div>Shipping Policy</div>
-			<div>FAQs</div>
-		</div>
-		<div class="link-containter">
-			<h2>Connect</h2>
-			<div>Facebook</div>
-			<div>Instagram</div>
-			<div>Pinterest</div>
-			<div>Physical Store</div>
-		</div>
+	<div>
+		<img src="../assets/logo-1.png">
+	</div>
+	<div>
+		<h2>Churi</h2>
+		<div>About Us</div>
+		<div>Contact Us</div>
+	</div>
+	<div>
+		<h2>Help</h2>
+		<div>Return Policy</div>
+		<div>Shipping Policy</div>
+		<div>FAQs</div>
+	</div>
+	<div>
+		<h2>Connect</h2>
+		<div><a href="https://www.facebook.com/ChuribyLz"><FacebookIcon />Facebook</a></div>
+		<div><a href="https://www.instagram.com/churi.ca/"><InstagramIcon />Instagram</a></div>
+		<div><a href="https://ca.pinterest.com/churonmi/"><PinterestIcon />Pinterest</a></div>
+		<div><a href="https://maps.app.goo.gl/w6juwyojQM6z3QaF8"><MapIcon />Physical Store</a></div>
 	</div>
 </footer>
 </template>
@@ -30,11 +35,12 @@
 h2 {
 	margin-bottom: 1em;
 }
-.flex-row {
+footer {
+	display: flex;
 	justify-content: space-between;
-	height: 200px;
+	align-items: flex-start;
 }
-.link-containter{
-	height: 100%;
+svg {
+	margin-right: 1em;
 }
 </style>
--- front-end/src/components/StoreHeader.vue
+++ front-end/src/components/StoreHeader.vue
@@ -7,16 +7,16 @@
 
 <template>
 <header>
-	<div class="regular-main-page-width header">
-		<SearchIcon />
-		<RouterLink to="/"><img src="../assets/logo-1.png" alt="Logo" class="centered-image"></RouterLink>
+	<div class="regular-main-page-width header-top">
+		<div><SearchIcon /></div>
+		<div><RouterLink to="/"><img src="../assets/logo-1.png" alt="Logo" class="centered-image"></RouterLink></div>
 		<div class="right-sub-container">
 			<ShoppingBagIcon />
 			<div class="drop-container" v-if="isSignedIn">
 				<div>UserPotrait</div>
 				<div>
 				<RouterLink to="/user-management"><div>Dashboard</div></RouterLink>
-					<div>Sign out</div>
+				<div>Sign out</div>
 				</div>
 			</div>
 			<div v-else class="right-sub-container">
@@ -25,7 +25,7 @@
 			</div>
 		</div>
 	</div>
-	<nav class="regular-main-page-width header">
+	<nav class="regular-main-page-width nav-bar">
 		<div>Home</div>
 		<div>App Products</div>
 		<div>Handmade</div>
@@ -50,7 +50,22 @@ export default {
 </script>
 
 <style scoped>
-.header {
+.header-top {
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+}
+.header-top > div {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+}
+.header-top > div:nth-child(2) {
+	justify-content: center;
+}
+.header-top > div:nth-child(3) {
+	justify-content: flex-end;
+}
+.nav-bar {
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
