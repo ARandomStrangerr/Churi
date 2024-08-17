9671fe9f26b9169f3e5bb8f95d60e7bd77a9db3d
thanhdo <billslim0996@gmail.com>
2024-08-16 17:19:49 -0700

    change fields that display products

--- front-end/src/components/UserManagementProductList.vue
+++ front-end/src/components/UserManagementProductList.vue
@@ -1,7 +1,8 @@
 <script setup>
 import SearchIcon from "./icons/Search.vue";
 import AddIcon from "./icons/Add.vue"
-import TableComponent from "./UserManagementTable.vue"
+import TrashBinIcon from "./icons/TrashBin.vue";
+import EditIcon from "./icons/Edit.vue";
 import { RouterLink, RouterView, useRoute } from "vue-router";
 import { notification } from "../stores/Notification";
 import Axios from "axios";
@@ -15,8 +16,34 @@ import Axios from "axios";
 		<RouterLink to="/user-management/create-product"><div class="button green"><AddIcon />Add Product</div></RouterLink>
 	</div>
 </div>
-<TableComponent :columnName="columnName" :columnKey="columnKey" :tableData="productList" @onEdit="onEditItem" @onDelete="onDeleteItem"/>
-<RouterView :message="'Confirm to delete the Item?'" @confirm="onConfirmDialogue" @decline="onDeclineDialogue"/>
+<table>
+	<thead>
+		<tr>
+			<th>Product ID</th>
+			<th>Product Name</th>
+			<th>Owner</th>
+			<th>Category</th>
+			<th>Published</th>
+			<th>Variant</th>
+			<th>Action</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr v-for="(product, index) in productList" :key="index">
+			<td>{{ product._id }}</td>
+			<td>{{ product.name }}</td>
+			<td>{{ product.ownerUsername }}</td>
+			<td>{{ product.category }}</td>
+			<td>{{ product.published }}</td>
+			<td>{{ product.variant }}</td>
+			<td>
+				<div class="button yellow" @click="onEditItem(index)"><EditIcon /></div>
+				<div class="button red" @click="onDeleteItem(index)"><TrashBinIcon /></div>
+			</td>
+		</tr>
+	</tbody>
+</table>
+<RouterView :message="message" @confirm="onConfirmDialogue" @decline="onDeclineDialogue"/>
 </template>
 
 <script>
@@ -24,9 +51,8 @@ export default {
 	inject: ["expressAddress"],
 	data() {
 		return {
-			productList: [],
-			columnName: ["Name", "Owner", "Price", "Stock", "Discount", "Action"],
-			columnKey: ["name", "ownerUsername", "price", "stock", "discount"]
+			message: "",
+			productList: []
 		}
 	},
 	methods: {
@@ -34,6 +60,7 @@ export default {
 			this.$router.push(`/user-management/edit-product/${this.productList[index]._id}`);
 		},
 		onDeleteItem(index) {
+			this.message = `Confirm to detete the product ${this.productList[index]._id} - ${this.productList[index].name}?`;
 			this.$router.push(`/user-management/product-list/delete-product/${this.productList[index]._id}`);
 		},
 		onConfirmDialogue() {
@@ -53,11 +80,12 @@ export default {
 	mounted() {
 		Axios.get(`${this.expressAddress}/user-management/get-product-list`
 		).then((data) => {
+			console.log(data.data);
 			this.productList = data.data;
 		}).catch((data) => {
 			notification().addNotification(data.response.data, "red");
 			this.$router.push("/sign-in");
-		})
+		});
 	}
 }
 </script>
@@ -75,5 +103,12 @@ export default {
 }
 .search-box{
 	margin-right: 1em;
+}
+.button {
+	border-radius: 4px;
+	margin-right: 1em;
+}
+.button:last-child{
+	margin-right: 0px;
 }
 </style>
